ab773a56c2435351b979c8c13c504e5ecc911e5b
Patrick Battazza <PATRICK.BATTAZZA@local.ittcesena.it>
2022-11-24 08:21:15 +0100

    LibroConMetodi

--- ProvaGit/Libro.cs
+++ ProvaGit/Libro.cs
@@ -13,5 +13,38 @@ namespace ProvaGit
         public int AnnoPubblicazione { get; set; }
         public string Editore { get; set; }
         public int Pagine { get; set; }
+
+        public Libro(string autore, string titolo, int annoPubblicazione, string editore, int pagine)
+        {
+            Autore = autore;
+            Titolo = titolo;
+            AnnoPubblicazione = annoPubblicazione;
+            Editore = editore;
+            Pagine = pagine;
+        }
+
+        public string toString()
+        {
+            return "Autore: " + Autore + ", Titolo: " + Titolo + ", Anno di pubblicazione: " + AnnoPubblicazione + ", Editore: " + Editore + ", Pagine: " + Pagine;
+        }
+
+        public string readingTime(int p)
+        {
+            if (p < 100)
+            {
+                return "1h";
+            }
+            else
+            {
+                if (p > 100 && p < 200)
+                {
+                    return "2h";
+                }
+                else
+                {
+                    return "> 2h";
+                }
+            }
+        }
     }
 }

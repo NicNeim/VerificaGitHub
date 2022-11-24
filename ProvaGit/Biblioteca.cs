cb0e31a7d4ad261dc3f5eb2517ac0887f71afd84
Patrick Battazza <PATRICK.BATTAZZA@local.ittcesena.it>
2022-11-24 08:28:00 +0100

    BibliotecaProprieta

---
+++ ProvaGit/Biblioteca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaGit
+{
+    internal class Biblioteca
+    {
+        public string Nome { get; set; }
+        public string Indirizzo { get; set; }
+        public string OrarioApertura { get; set; }
+        public string OrarioChiusura { get; set; }
+        public List<Libro> ElencoLibri { get; set; }
+    }
+}

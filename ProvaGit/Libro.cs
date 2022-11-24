9718c25fee8569142b022c61bf3b1b6d6cb19a7a
Patrick Battazza <PATRICK.BATTAZZA@local.ittcesena.it>
2022-11-24 08:17:21 +0100

    LibroProprieta

---
+++ ProvaGit/Libro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaGit
+{
+    internal class Libro
+    {
+        public string Autore { get; set; }
+        public string Titolo { get; set; }
+        public int AnnoPubblicazione { get; set; }
+        public string Editore { get; set; }
+        public int Pagine { get; set; }
+    }
+}
